5b96230be4662259ede1f0b05b06c0cb49ca65bd
tobslob <kazmobileapp@gmail.com>
2019-10-12 15:45:28 +0100

    feat(Starships): Create starships layouts and views

---
+++ src/components/views/Starships.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="starships">
+        <div id="header">
+          <h1>Popular Starships</h1>
+        </div>
+        <Starships 
+        :starships="starships"
+        />
+        <StarshipMore />
+    </div>
+</template>
+
+<script>
+import Starships from '../../components/layouts/Starships.layout';
+import StarshipMore from '../layouts/starships.layouts.more';
+export default {
+    props: {
+        starships: Array
+    },
+    components: {
+        Starships,
+        StarshipMore
+    }
+}
+</script>
+
+<style scoped>
+.starships{
+    text-align: center;
+}
+.starships h1{
+    display: inline;
+    color: #494956;
+}
+#header{
+    margin-bottom: 80px
+}
+</style>
